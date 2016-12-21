d65d189a355fd4f8a55caee38ed6e4603f4b6a00
Max Leblanc <mleblanc@poka.io>
2016-12-21 14:25:08 -0500

    Better entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM ubuntu:16.04
 
 CMD ["/entrypoint.sh"]
-VOLUME ["/rclone/config"]
+VOLUME ["/config"]
 
 RUN apt-get update && apt-get -y upgrade
 RUN apt-get install -y python-pip python-dev build-essential unzip curl
 
+RUN mkdir /rclone
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mkdir /rclone
 curl http://downloads.rclone.org/rclone-current-linux-amd64.zip -o /rclone/rclone-current-linux-amd64.zip
 unzip /rclone/rclone-current-linux-amd64.zip -d /rclone
 rm /rclone/rclone-current-linux-amd64.zip
 cd /rclone
 RCLONE_FOLDER=(*)
-mv /rclone/$RCLONE_FOLDER/* /rclone
-ls /rclone
+echo "/rclone/$RCLONE_FOLDER"
+/rclone/$RCLONE_FOLDER
