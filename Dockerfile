73b0fe199c42deac87ff79101c53994dc72dc0e0
Max Leblanc <mleblanc@poka.io>
2017-01-04 11:07:05 -0500

    Added support for source subdirectory

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:16.04
 
-CMD ["/entrypoint.sh"]
+CMD ["/main.sh"]
 VOLUME ["/config"]
 VOLUME ["/log"]
 
@@ -8,5 +8,5 @@ RUN apt-get update && apt-get -y upgrade
 RUN apt-get install -y python-pip python-dev build-essential unzip curl
 
 RUN mkdir /rclone
-COPY entrypoint.sh /entrypoint.sh
+COPY main.sh /main.sh
 RUN chmod +x entrypoint.sh
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl http://downloads.rclone.org/rclone-current-linux-amd64.zip -o /rclone/rclone-current-linux-amd64.zip
-unzip /rclone/rclone-current-linux-amd64.zip -d /rclone
-rm /rclone/rclone-current-linux-amd64.zip
-cd /rclone
-RCLONE_FOLDER=(*)
-echo "/rclone/$RCLONE_FOLDER"
-/rclone/$RCLONE_FOLDER/rclone --config=/config/rclone.conf --log-file=/log/rclone.log $EXTRAPARAMS copy $SOURCE:/ $DESTINATION:$BUCKET/
